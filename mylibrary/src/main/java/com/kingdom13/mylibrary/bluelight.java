080cb437df7d707b425346c672d4c1e51808ef88
inhart <3300267+inhart@users.noreply.github.com>
2025-08-23 01:22:51 +0200

    connnect

--- mylibrary/src/main/java/com/kingdom13/mylibrary/bluelight.java
+++ mylibrary/src/main/java/com/kingdom13/mylibrary/bluelight.java
@@ -1,11 +1,15 @@
 package com.kingdom13.mylibrary;
 
+import static android.content.ContentValues.TAG;
+
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 
+import android.bluetooth.BluetoothSocket;
 import android.bluetooth.le.BluetoothLeScanner;
+
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.Context;
@@ -13,8 +17,12 @@ import android.util.Log;
 
 import androidx.annotation.RequiresPermission;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+
+import kotlin.jvm.JvmStatic;
 
 public class bluelight {
 
@@ -22,7 +30,11 @@ public class bluelight {
     private static BluetoothLeScanner bleScanner;
     private static final List<String> devices = new ArrayList<>();
 
+    private static final UUID UUID = java.util.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private static BluetoothSocket bluetoothSocket;
     // Inicializar Bluetooth
+    @JvmStatic
     public static boolean initBluetooth(Context context) {
         BluetoothManager bluetoothManager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
@@ -41,6 +53,7 @@ public class bluelight {
 
     // Escanear dispositivos BLE
     @RequiresPermission(Manifest.permission.BLUETOOTH_SCAN)
+    @JvmStatic
     public static void startScan() {
         if (bleScanner == null) {
             Log.e("MyUnityModule", "BLE Scanner no disponible.");
@@ -53,6 +66,7 @@ public class bluelight {
 
     // Detener escaneo BLE
     @RequiresPermission(Manifest.permission.BLUETOOTH_SCAN)
+    @JvmStatic
     public static void stopScan() {
         if (bleScanner != null) {
             bleScanner.stopScan(scanCallback);
@@ -63,6 +77,73 @@ public class bluelight {
     // Obtener lista de dispositivos encontrados
     public static String[] getFoundDevices() {
         return devices.toArray(new String[0]);
+    }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_SCAN)
+    public static void connect(String deviceAddress){
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+        if (device == null) {
+            Log.e(TAG, "Dispositivo no encontrado.");
+            return;
+        }
+else try {
+            BluetoothSocket socket = device.createRfcommSocketToServiceRecord(UUID);
+            socket.connect();
+            Log.i(TAG, "Conectado");
+        } catch (IOException e) {
+            Log.e(TAG, "Error al conectar: " + e.getMessage());
+
+        }
+    }
+
+    private static void disconnect() {
+        if (bluetoothSocket != null) {
+            try {
+                bluetoothSocket.close();
+                Log.i(TAG, "Desconectado");
+            } catch (IOException e) {
+                Log.e(TAG, "Error al desconectar: " + e.getMessage());
+            }
+        }
+    }
+
+    public static boolean pairDevice(String deviceAddress) {
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+        if (device == null) {
+            Log.e("MyUnityModule", "Dispositivo no encontrado.");
+            return false;
+        }
+
+        try {
+            device.createBond();
+            Log.d("MyUnityModule", "Dispositivo parqueado.");
+            return true;
+        } catch (SecurityException e) {
+            Log.e("MyUnityModule", "Error al intentar parquear el dispositivo: " + e.getMessage());
+            return false;
+
+        }
+    }
+
+    public static void connectToPairedDevice(String deviceAddress) {
+        BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+        if (device == null) {
+            Log.e("MyUnityModule", "Dispositivo no encontrado.");
+
+        }else{
+            try {
+                BluetoothSocket socket = device.createRfcommSocketToServiceRecord(java.util.UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+                socket.connect();
+                Log.d("MyUnityModule", "Conexión establecida con el dispositivo.");
+            } catch (IOException e) {
+                Log.e("MyUnityModule", "Error al establecer la conexión: " + e.getMessage());
+
+            }
+        }
+
+
+
+
     }
 
     private static final ScanCallback scanCallback = new ScanCallback() {
